abed028085d711d5f7f927837f80ee3b46194ded
Tobias Andersen <tobias.andersen111@gmail.com>
2015-11-12 03:13:31 +0100

    Removed API code

--- api/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-from models import Session
-
-
-app = Flask('btcex')
-app.config['SECRET_KEY'] = 'secret key here'
-session = Session()
--- api/views.py
+++
@@ -1,4 +0,0 @@
-import simplejson as json
-from flask import request, jsonify
-
-from api import app, session
